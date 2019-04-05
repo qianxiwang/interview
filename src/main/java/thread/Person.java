3fe1b496fb1086b5c921550d044048d3ced01f5d
mac <2734756709@qq.com>
2019-04-05 22:58:05 +0800

    生产者和消费者(同步问题)

--- src/main/java/thread/Person.java
+++ src/main/java/thread/Person.java
@@ -5,8 +5,19 @@ public class Person {
     private String name;
     private String sex;
 
+    private Boolean isEmpty = true;
 
-    public void set(String name, String sex) {
+    //生产
+    public synchronized void set(String name, String sex) {
+
+        while (!isEmpty) { //不为空，生产者应该停下来，等待消费者去消费
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         this.name = name;
 
         try {
@@ -16,10 +27,27 @@ public class Person {
         }
 
         this.sex = sex;
+
+        System.out.println("生产：" + getName() + ":" + getSex());
+
+        isEmpty = false;//不为空
+        this.notifyAll(); //唤醒消费者，起来消费了
     }
 
-    public void get() {
+    //消费
+    public synchronized void get() {
+        while (isEmpty) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         System.out.println("消费：" + getName() + ":" + getSex());
+
+        isEmpty = true;
+        this.notifyAll();//唤醒生产者，应该生产了
     }
 
     public String getName() {
